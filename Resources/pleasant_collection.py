96435b4a78dd65c9bdd11e6f084bf1074d8dd5a9
Raul Alonzo <ram2489@gmail.com>
2020-06-19 21:22:06 -0500

    Finished a basic logic to count positive and negative words

--- Resources/pleasant_collection.py
+++ Resources/pleasant_collection.py
@@ -1,16 +1,27 @@
-pleasant = {
-    'open': ['understanding','confident','reliable','easy','amazed','free','sympathetic','interested','satisfied',
-            'receptive','accepting','kind'],
-    'happy':['great','joyous','lucky','fortunate','delighted','overjoyed','gleeful','thankful','important',
-            'festive','ecstatic','glad','cheerful','elated','jubilant'],
-    'alive':['playful','courageous','energetic','liberated','optimistic',
-            'impulsive','free','animated','spirited','thrilled','wonderful'],
-    'good':['calm','peaceful','at ease','comfortable','pleased','encouraged',
-            'clever','surprised','content','quiet','certain','relaxed','serene','reassured'],
-    'love': ['loving','considerate','affectionate', 'sensitive','tender','devoted','attracted','passionate','admiration','warm','touched','close','comforted','loved'],
-'interested':['concerned','affected','fascinated','intrigued','absorbed','inquisitive','engrossed','curious','drawn toward'],
-'positive':['eager','keen','earnest','intent','inspired','determined','excited','enthusiastic','bold','brave','daring',
-            'optimistic'],
-'strong': ['impulsive','free','sure','certain','rebellious','unique','dynamic','tenacious','hardy','secure',
-        'confident','challenged']
-        }
+# pleasant = {
+#     'open': ['understanding','confident','reliable','easy','amazed','free','sympathetic','interested','satisfied',
+#             'receptive','accepting','kind'],
+#     'happy':['great','joyous','lucky','fortunate','delighted','overjoyed','gleeful','thankful','important',
+#             'festive','ecstatic','glad','cheerful','elated','jubilant'],
+#     'alive':['playful','courageous','energetic','liberated','optimistic',
+#             'impulsive','free','animated','spirited','thrilled','wonderful'],
+#     'good':['calm','peaceful','at ease','comfortable','pleased','encouraged',
+#             'clever','surprised','content','quiet','certain','relaxed','serene','reassured'],
+#     'love': ['loving','considerate','affectionate', 'sensitive','tender','devoted','attracted','passionate','admiration','warm','touched','close','comforted','loved'],
+# 'interested':['concerned','affected','fascinated','intrigued','absorbed','inquisitive','engrossed','curious','drawn toward'],
+# 'positive':['eager','keen','earnest','intent','inspired','determined','excited','enthusiastic','bold','brave','daring',
+#             'optimistic'],
+# 'strong': ['impulsive','free','sure','certain','rebellious','unique','dynamic','tenacious','hardy','secure',
+#         'confident','challenged']
+#         }
+
+pleasant = ['understanding','confident','reliable','easy','amazed','free','sympathetic','interested','satisfied',
+            'receptive','accepting','kind', 'great','joyous','lucky','fortunate','delighted','overjoyed','gleeful','thankful','important',
+            'festive','ecstatic','glad','cheerful','elated','jubilant', 'playful','courageous','energetic','liberated','optimistic',
+            'impulsive','free','animated','spirited','thrilled','wonderful','calm','peaceful','at ease','comfortable','pleased','encouraged',
+            'clever','surprised','content','quiet','certain','relaxed','serene','reassured', 'loving','considerate','affectionate', 'sensitive',
+            'tender','devoted','attracted','passionate','admiration','warm','touched','close','comforted','loved','concerned','affected','fascinated',
+            'intrigued','absorbed','inquisitive','engrossed','curious','drawn toward','eager','keen','earnest','intent','inspired','determined','excited',
+            'enthusiastic','bold','brave','daring',
+            'optimistic','impulsive','free','sure','certain','rebellious','unique','dynamic','tenacious','hardy','secure',
+        'confident','challenged'];
